b2caa9afb4719a7e1818b82b9836681ab4d23e6d
Javier Pomachagua <javierpp0303@gmail.com>
2018-10-11 06:22:58 -0500

    Lecture 24 - 28

--- src/components/header.js
+++ src/components/header.js
@@ -1,10 +1,33 @@
 //Import libraries for making a component
 import React from 'react';
-import { Text } from 'react-native';
+import { Text, View } from 'react-native';
 
 //Make a component
-const Header = () => {
-  return <Text>Albums</Text>;
+const Header = (props) => {
+  const { textStyle, viewStyle } = styles;
+  return (
+    <View style={viewStyle}>
+      <Text style={textStyle}>{props.headerText}</Text>
+    </View>    
+  );
+};
+
+const styles = {
+  viewStyle: {
+    backgroundColor: '#F8F8F8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 20 },
+    shadowOpacity: 0.9,
+    elevation: 10,
+    position: 'relative'
+  },
+  textStyle: {
+    fontSize: 20
+  }
 };
 
 
